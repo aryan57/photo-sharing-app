2e837ca49b7e16bc0acf10443cd5169db2cdbcce
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-06-20 04:10:52 +0530

    fix version

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/storage"
-import '@firebase/app-check';
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
