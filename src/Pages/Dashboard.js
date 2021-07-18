36bff4e84fb59c3d527717964553542d931cf5fd
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2021-07-18 23:27:40 +0530

    add background image

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Header from '../Widgets/Header'
-// import { Card, Container, Image, Button, Badge } from 'react-bootstrap'
+import { Container, Alert } from 'react-bootstrap'
 
 
 export default function Dashboard() {
@@ -9,11 +9,19 @@ export default function Dashboard() {
   return (
     <>
       <Header />
+      <div style={{ backgroundImage: "url(/dashboard-background.jpg)", height: "100vh", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+        <Container className="d-flex align-items-center justify-content-center" >
+          <div className="w-100" style={{ maxWidth: "450px", marginTop: 50 }}>
+            <Alert variant="primary" >
+              <h3>
+                Kindly go to different modules (like <a href="/grid">grid</a>,<a href="/profile">profile</a>) to see them.
+              </h3>
+            </Alert>
 
-      {/* <Container className="d-flex align-items-center justify-content-center">
-        <div className="w-100" style={{ maxWidth: "450px", marginTop: 50 }}>
-        </div>
-      </Container> */}
+          </div>
+        </Container>
+
+      </div>
 
     </>
   )
